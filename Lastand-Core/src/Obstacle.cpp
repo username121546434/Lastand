d32429b3fcbefed244145127440907a110d694b9
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-12-24 19:16:05 -0500

    Made loading map from file work and fixed map1.txt

--- Lastand-Core/src/Obstacle.cpp
+++ Lastand-Core/src/Obstacle.cpp
@@ -19,13 +19,14 @@ std::vector<Obstacle> load_from_file(const std::string &file_name) {
     while (std::getline(file, curr_line)) {
         std::stringstream ss(curr_line);
         uint16_t x, y, width, height;
-        uint8_t r, g, b;
+        unsigned short r, g, b, a;
         char delimiter; // To skip commas
 
         // Parse the line
         if (ss >> x >> delimiter >> y >> delimiter >> width >> delimiter 
-               >> height >> delimiter >> r >> delimiter >> g >> delimiter >> b) {
-            Obstacle obstacle{x, y, width, height, {r, g, b}};
+               >> height >> delimiter >> r >> delimiter >> g >> delimiter >> b >> delimiter >> a) {
+            std::cout << "Obstacle: " << x << ", " << y << ", " << width << ", " << height << ", " << r << ", " << g << ", " << b << ", " << a << std::endl;
+            Obstacle obstacle{x, y, width, height, {(uint8_t)r, (uint8_t)g, (uint8_t)b, (uint8_t)a}};
             obstacles.push_back(obstacle);
         }
     }
