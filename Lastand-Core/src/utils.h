2c8831e2d3f79318bfc1c7a27200239b997baea2
Atharv Gokule <106445077+username121546434@users.noreply.github.com>
2024-12-25 17:36:43 -0500

    Made operator<< work with std::array and print out hex values

--- Lastand-Core/src/utils.h
+++ Lastand-Core/src/utils.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <array>
 #include <cstdint>
 #include <ostream>
 #include <vector>
@@ -9,10 +10,25 @@ struct Color {
 
 template <typename T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
-    os << "{ ";
+    std::ios_base::fmtflags f( os.flags() );  // save flags state
+    os << "vec{ " << std::hex;
     for (auto i: v)
         os << (int)i << ' ';
     os << '}';
+    os.flags(f);
+
+    return os;
+}
+
+template <typename T, typename std::size_t n>
+std::ostream &operator<<(std::ostream &os, const std::array<T, n> &v) {
+    std::ios_base::fmtflags f( os.flags() );  // save flags state
+    os << "arr{ " << std::hex;
+    for (auto i: v)
+        os << (int)i << ' ';
+    os << '}';
+    os.flags(f);
+
     return os;
 }
 
